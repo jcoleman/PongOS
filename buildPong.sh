b068192d812dcb353d254458cd9d1005d691706a
jcoleman <jtc331@gmail.com>
2024-08-12 21:12:18 -0400

    Update floppy image generation
    `oseek` isn't in Linux's `dd`; always generate a 1.44 MB floppy.

--- buildPong.sh
+++ buildPong.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 nasm -o bootloader.bin -f bin bootloader.asm 
 nasm -o kernel.bin -f bin kernel.asm 
-dd if=bootloader.bin of=floppy.img bs=512
-dd if=kernel.bin of=floppy.img bs=512 oseek=1
+# make a completely blank disk image
+dd if=/dev/zero of=floppy.img bs=512 count=2880
+dd if=bootloader.bin of=floppy.img bs=512 count=1 conv=notrunc
+dd if=kernel.bin of=floppy.img bs=512 seek=1 conv=notrunc
